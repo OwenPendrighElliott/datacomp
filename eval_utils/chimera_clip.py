57f09036992a464958319360b813cc537e31c7f1
OwenPendrighElliott <owenpendrighelliott@gmail.com>
2024-09-16 11:09:04 +1000

    fix image preprocessing in chimnera clip

--- eval_utils/chimera_clip.py
+++ eval_utils/chimera_clip.py
@@ -69,7 +69,8 @@ class ChimeraCLIP():
     def e2e_encode_image(self, images, normalize: bool = True):
         latents = []
         for model, transform in zip(self.clip_models, self.preprocessors):
-            latent = model.encode_image(transform(images).to(self.device), normalize=normalize)
+            images = [transform(img).to(self.device) for img in images]
+            latent = model.encode_image(images, normalize=normalize)
             latents.append(latent)
         
         concat = torch.cat(latents, dim=-1)
